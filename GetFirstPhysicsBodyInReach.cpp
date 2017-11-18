4f47842e9c6744447334c5eb5843d08da6a724c7
Ramachandra Junior <ajaykumarch33@yahoo.com>
2017-11-18 20:59:28 +0530

    Add First physics body in reach script

---
+++ GetFirstPhysicsBodyInReach.cpp
@@ -0,0 +1,26 @@
+#define OUT
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach()
+{    
+    FHitResult HitResult;
+    FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+    
+    // reach line start and end to find objects on
+    FVector Start = GetReachLineStart();
+    FVector End = GetReachLineEnd();
+
+    /// Line-trace (AKA ray-cast) out to reach distance
+    /// ===============================================
+    bool bFoundHit =
+        GetWorld()->LineTraceSingleByObjectType(
+            OUT HitResult,
+            Start,
+            End,
+            FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+            TraceParameters
+        );
+    /// ===============================================
+
+
+    return HitResult;
+}
